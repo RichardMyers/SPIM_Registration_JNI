47aa69eb554279fc48b882a6d0ed85f5b397775f
Richard Myers <richard@intelligent-imaging.com>
2016-08-19 22:49:14 +0200

    Added tests for different paths for computing DoG
    DifferenceOfGaussian seems to work, but .2 threshold gives same result
    as .8 threshold when I test in Fiji.  The interactive DoG method doesn't
    seem to work for me at all.

--- src/main/java/com/iii/jni/spim/DifferenceOfGaussianJNI.java
+++ src/main/java/com/iii/jni/spim/DifferenceOfGaussianJNI.java
@@ -1,14 +1,20 @@
 package com.iii.jni.spim;
 
 import com.sun.tools.javac.util.Assert;
+import mpicbg.imglib.algorithm.scalespace.DifferenceOfGaussianPeak;
+import mpicbg.imglib.algorithm.scalespace.DifferenceOfGaussianReal1;
+import mpicbg.imglib.algorithm.scalespace.SubpixelLocalization;
 import mpicbg.imglib.container.array.ArrayContainerFactory;
 import mpicbg.imglib.cursor.LocalizableByDimCursor;
+import mpicbg.imglib.cursor.LocalizableCursor;
 import mpicbg.imglib.image.Image;
 import mpicbg.imglib.image.ImageFactory;
+import mpicbg.imglib.outofbounds.OutOfBoundsStrategyValueFactory;
 import mpicbg.imglib.type.numeric.real.FloatType;
 import mpicbg.imglib.wrapper.ImgLib2;
 import mpicbg.spim.io.IOFunctions;
-import net.imglib2.RandomAccessibleInterval;
+import mpicbg.spim.registration.detection.DetectionSegmentation;
+import net.imglib2.*;
 import net.imglib2.algorithm.gauss3.Gauss3;
 import net.imglib2.exception.IncompatibleTypeException;
 import net.imglib2.img.Img;
@@ -29,84 +35,144 @@ import java.util.*;
 
 /**
  * Created by Richard on 8/16/2016.
+ *
+ * Test for trying to reproduce via direct JNI calls the results of interactive interest point discovery via the
+ * Difference-of-Gaussian routines in the FIJI plugin SPIM_Registration.
+ *
+ * Two methods were tried, DifferenceOfGaussian.compute and InteractiveDoG.updatePreview
+ *
  */
 public class DifferenceOfGaussianJNI {
 
     // call this method from JNI, returns array with {x,y} sub-pixel points of interest
-    public static float[] compute(float[] inImage, final int width, final int height,
+    public static float[] compute(final float[] inImage, final int width, final int height,
                                   final float calXY, final float calZ,
                                   final int downsampleXY, final int downsampleZ,
-                                  final float sigma, final float threshold)
+                                  final float sigma1, final float threshold,
+                                  final boolean useInteractiveDoGMethod)
     {
-        ArrayList<InterestPoint> interestPoints;
-
+        float[] interestPointsArray = null;
         try {
-            IOFunctions.println("HERE1");
-
-            // create 'imageArray' which wraps 'inImage' float array
-            net.imglib2.img.basictypeaccess.array.FloatArray imageArray = new FloatArray(inImage);
-
-            IOFunctions.println("HERE2");
-
-            // create 'data' ArrayImg from 'imageArray'
-            ArrayImg<net.imglib2.type.numeric.real.FloatType, net.imglib2.img.basictypeaccess.array.FloatArray> data;
-            data = new ArrayImg<>(imageArray, (new long[]{width, height}), new net.imglib2.util.Fraction());
-
-            // down sample 'data' to create 'input'
-            final AffineTransform3D affineTransform = new AffineTransform3D();
-            final RandomAccessibleInterval<net.imglib2.type.numeric.real.FloatType> input =
-                    downsample(data, calXY, calZ, downsampleXY, downsampleZ, affineTransform);
-            IOFunctions.println("HERE3");
-
-            // pre smooth data
-            double additionalSigmaX = 0.0;
-            double additionalSigmaY = 0.0;
-            double additionalSigmaZ = 0.0;
-            preSmooth(input, additionalSigmaX, additionalSigmaY, additionalSigmaZ);
-
-            // wrap 'input' to create imglib1 'img'
-            final Image<FloatType> img = ImgLib2.wrapFloatToImgLib1((Img<net.imglib2.type.numeric.real.FloatType>) input);
-
-            IOFunctions.println("HERE4");
-
-            interestPoints = ProcessDOG.compute(
-                    null, // cuda
-                    null, // deviceList
-                    false, // accurateCUDA
-                    0, // percentGPUMem
-                    img,
-                    (Img<net.imglib2.type.numeric.real.FloatType>) input,
-                    sigma,
-                    threshold,
-                    1, // localization = quadratic
-                    0.5, // imageSigmaX
-                    0.5, // imageSigmaY
-                    0.5, // imageSigmaZ
-                    false, // findMin
-                    true, // findMax
-                    0.0, // minIntensity
-                    65535.0, // maxIntensity
-                    false // keepIntensity
-            );
-
-            float[] interestPointsArray = new float[interestPoints.size()*2];
-            for (int i = 0; i < interestPoints.size(); i++) {
-                interestPointsArray[i*2] = interestPoints.get(i).getFloatPosition(0);
-                interestPointsArray[i*2+1] = interestPoints.get(i).getFloatPosition(0);
+            if (useInteractiveDoGMethod) {
+                // use example from InteractiveDoG.java
+                interestPointsArray = InteractiveDoG(inImage, width, height, sigma1, threshold);
+            } else {
+                // use example from DifferenceOfGaussian.java
+                interestPointsArray = DifferenceOfGaussian(inImage, width, height,
+                        calXY, calZ, downsampleXY, downsampleZ, sigma1, threshold);
             }
-
-            IOFunctions.println("HERE5");
-            return interestPointsArray;
-        }
-        catch( Exception e )
-        {
-            IOFunctions.println( "DifferenceOfGaussianJNI:: failed to compute. " + e );
+        } catch (Exception e) {
+            IOFunctions.println("DifferenceOfGaussianJNI.compute:: failed. " + e);
             e.printStackTrace();
         }
 
-        IOFunctions.println("HERE6");
+        return interestPointsArray;
+    }
 
-        return null;
+    public static float[] InteractiveDoG(final float[] inImage, final int width, final int height,
+                                         final float sigma1, final float threshold)
+    {
+        IOFunctions.println( "Using DifferenceOfGaussian method.");
+
+        Image<FloatType> img = new ImageFactory<FloatType>(new FloatType(), new ArrayContainerFactory()).
+                createImage(new int[]{width, height});
+
+        final int[] location = new int[2];
+        final LocalizableCursor<FloatType> cursor = img.createLocalizableCursor();
+        final long[] pos = new long[2];
+        while (cursor.hasNext()) {
+            cursor.fwd();
+            cursor.getPosition(location);
+            final long index = location[1] * width + location[0];
+            cursor.getType().set(inImage[(int) index]);
+        }
+
+        // interactive DoG
+        final float k, K_MIN1_INV;
+        final float[] sigma, sigmaDiff;
+        int sensitivity = 4;
+        float imageSigma = 0.5f;
+        float thresholdMin = 0.0001f;
+
+        k = (float) DetectionSegmentation.computeK(sensitivity);
+        K_MIN1_INV = DetectionSegmentation.computeKWeight(k);
+        sigma = DetectionSegmentation.computeSigma(k, sigma1);
+        sigmaDiff = DetectionSegmentation.computeSigmaDiff(sigma, imageSigma);
+
+        // the upper boundary
+        float sigma2 = sigma[1];
+
+        final DifferenceOfGaussianReal1<FloatType> dog =
+                new DifferenceOfGaussianReal1<FloatType>(img, new OutOfBoundsStrategyValueFactory<FloatType>(), sigmaDiff[0], sigmaDiff[1], threshold / 4, K_MIN1_INV);
+        dog.setKeepDoGImage(true);
+        dog.process();
+
+        final SubpixelLocalization<FloatType> subpixel = new SubpixelLocalization<FloatType>(dog.getDoGImage(), dog.getPeaks());
+        subpixel.process();
+
+        ArrayList<DifferenceOfGaussianPeak<FloatType>> peaks;
+        peaks = dog.getPeaks();
+
+        float[] interestPointsArray = new float[peaks.size() * 2];
+        for (int i = 0; i < peaks.size(); i++) {
+            interestPointsArray[i * 2] = peaks.get(i).get(0);
+            interestPointsArray[i * 2 + 1] = peaks.get(i).get(1);
+        }
+
+        return interestPointsArray;
+    }
+
+    public static float[] DifferenceOfGaussian(final float[] inImage, final int width, final int height,
+                                               final float calXY, final float calZ,
+                                               final int downsampleXY, final int downsampleZ,
+                                               final float sigma1, final float threshold)
+    {
+        IOFunctions.println( "Using DifferenceOfGaussian method.");
+
+        Img<net.imglib2.type.numeric.real.FloatType> data;
+        data = makeImage(new net.imglib2.type.numeric.real.FloatType(), inImage, (new long[]{width, height}));
+
+        // down sample 'data' to create 'input'
+        final AffineTransform3D affineTransform = new AffineTransform3D();
+        final RandomAccessibleInterval<net.imglib2.type.numeric.real.FloatType> input =
+                downsample(data, calXY, calZ, downsampleXY, downsampleZ, affineTransform);
+
+        // pre smooth data
+        double additionalSigmaX = 0.0;
+        double additionalSigmaY = 0.0;
+        double additionalSigmaZ = 0.0;
+        preSmooth(input, additionalSigmaX, additionalSigmaY, additionalSigmaZ);
+
+        // wrap 'input' to create imglib1 'img'
+        final Image<FloatType> img = ImgLib2.wrapFloatToImgLib1((Img<net.imglib2.type.numeric.real.FloatType>) data);
+
+        ArrayList< InterestPoint >interestPoints = ProcessDOG.compute(
+                null, // cuda
+                null, // deviceList
+                false, // accurateCUDA
+                0, // percentGPUMem
+                img,
+                (Img<net.imglib2.type.numeric.real.FloatType>) data,
+                sigma1,
+                threshold,
+                1, // localization = quadratic
+                0.5, // imageSigmaX
+                0.5, // imageSigmaY
+                0.5, // imageSigmaZ
+                false, // findMin
+                true, // findMax
+                0.0, // minIntensity
+                65535.0, // maxIntensity
+                false // keepIntensity
+        );
+
+        float[] interestPointsArray = new float[interestPoints.size() * 2];
+        for (int i = 0; i < interestPoints.size(); i++) {
+            interestPointsArray[i * 2] = interestPoints.get(i).getFloatPosition(0);
+            interestPointsArray[i * 2 + 1] = interestPoints.get(i).getFloatPosition(1);
+        }
+
+        return interestPointsArray;
     }
 
     public static void main(String[] args) {
@@ -129,12 +195,14 @@ public class DifferenceOfGaussianJNI {
                 image[i] = 2000f  + (rand.nextFloat() * 100f);
             }
             else {
-                image[i] = 0f;
+                image[i] = 1f;
             }
         }
 
+        boolean useInteractiveDoGMethod = false;
+
         float[] interestPointsArray =
-                compute(image, width, height, calXY, calZ, downsampleXY, downsampleZ, sigma, threshold);
+                compute(image, width, height, calXY, calZ, downsampleXY, downsampleZ, sigma, threshold, useInteractiveDoGMethod);
 
         int theExpectedPeaks = (width/64 * height/64);
         IOFunctions.println( "theExpectedPeakers = " + theExpectedPeaks );
@@ -214,7 +282,7 @@ public class DifferenceOfGaussianJNI {
 
     //protected static RandomAccessibleInterval< net.imglib2.type.numeric.real.FloatType > downsample(
     protected static RandomAccessibleInterval<net.imglib2.type.numeric.real.FloatType> downsample(
-            ArrayImg<net.imglib2.type.numeric.real.FloatType, ?> input,
+            Img<net.imglib2.type.numeric.real.FloatType> input,
             final float calXY,
             final float calZ,
             int downsampleXY,
@@ -237,7 +305,8 @@ public class DifferenceOfGaussianJNI {
 
         t.identity();
 
-        final ImgFactory<net.imglib2.type.numeric.real.FloatType> f = (input).factory();
+        final ImgFactory< net.imglib2.type.numeric.real.FloatType > f =
+                ((Img<net.imglib2.type.numeric.real.FloatType>)input).factory();
         RandomAccessibleInterval<net.imglib2.type.numeric.real.FloatType> output = input;
 
         t.set(downsampleXY, 0, 0);
@@ -255,6 +324,52 @@ public class DifferenceOfGaussianJNI {
 
         return output;
     }
+
+    /*
+    //protected static RandomAccessibleInterval< net.imglib2.type.numeric.real.FloatType > downsample(
+    protected static RandomAccessibleInterval<FloatType> downsample(
+            Image<FloatType> input,
+            final float calXY,
+            final float calZ,
+            int downsampleXY,
+            final int downsampleZ,
+            final AffineTransform3D t) {
+        // downsampleXY == 0 : a bit less then z-resolution
+        // downsampleXY == -1 : a bit more then z-resolution
+        if (downsampleXY < 1)
+            downsampleXY = downsampleFactor(downsampleXY, downsampleZ, calXY, calZ);
+
+        if (downsampleXY > 1)
+            IOFunctions.println("(" + new Date(System.currentTimeMillis()) + "): Downsampling in XY " + downsampleXY + "x ...");
+
+        if (downsampleZ > 1)
+            IOFunctions.println("(" + new Date(System.currentTimeMillis()) + "): Downsampling in Z " + downsampleZ + "x ...");
+
+        int dsx = downsampleXY;
+        int dsy = downsampleXY;
+        int dsz = downsampleZ;
+
+        t.identity();
+
+        final ImageFactory< FloatType > f = input.getImageFactory();
+        RandomAccessibleInterval<FloatType> output = input;
+
+        t.set(downsampleXY, 0, 0);
+        t.set(downsampleXY, 1, 1);
+        t.set(downsampleZ, 2, 2);
+
+        for (; dsx > 1; dsx /= 2)
+            output = Downsample.simple2x(input, f, new boolean[]{true, false, false});
+
+        for (; dsy > 1; dsy /= 2)
+            output = Downsample.simple2x(input, f, new boolean[]{false, true, false});
+
+        for (; dsz > 1; dsz /= 2)
+            output = Downsample.simple2x(input, f, new boolean[]{false, false, true});
+
+        return output;
+    }
+    */
 
     protected static <T extends RealType<T>> void preSmooth(final RandomAccessibleInterval<T> img,
                                                             final double additionalSigmaX,
